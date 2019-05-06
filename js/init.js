97056998b03af160c4a843ccba24daf3125d22ca
basma-zeyada <basma_zeyada@live.com>
2019-05-06 13:50:47 +0200

    styles

--- js/init.js
+++ js/init.js
@@ -1,3 +1,5 @@
+
+
 /*-----------------------------------------------------------------------------------*/
 /*  PORTFOLIO
 /*-----------------------------------------------------------------------------------*/
@@ -67,7 +69,7 @@ $(window).scroll(function() {
         }
     });
 
-  	if  ( ($(document).height() - $(window).height()) - $(window).scrollTop() < 1000 ){
+  	if  ( ($(document).height() - $(window).height()) - $(window).scrollTop() < 800 ){
 	    $('#footer-wrapper').css('z-index','4');
 	} else {
 		$('#footer-wrapper').css('z-index','1');
@@ -84,7 +86,7 @@ jQuery(document).ready(function($){
   } else {
       $('.navbar-fixed-top').addClass('opaqued');
   }
-  if  ( ($(document).height() - $(window).height()) - $(window).scrollTop() < 1000 ){
+  if  ( ($(document).height() - $(window).height()) - $(window).scrollTop() < 1400 ){
       $('#footer-wrapper').css('z-index','4');
     } else {
       $('#footer-wrapper').css('z-index','1');
